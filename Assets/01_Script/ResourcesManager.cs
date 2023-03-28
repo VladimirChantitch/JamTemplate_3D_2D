8f71773475a8818e6e85d8b775566ec77100b639
Vladimir Chantitch <chantitch.vladimir@gmail.com>
2023-03-28 16:13:31 +0200

    Comments

--- Assets/01_Script/ResourcesManager.cs
+++ Assets/01_Script/ResourcesManager.cs
@@ -2,11 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// This class is their for you get info about your data
+/// ex : scene state, so, ect... 
+/// Its a singleton only one can be activated at a time
+/// </summary>
 public class ResourcesManager : MonoBehaviour
 {
     #region singleton
     public static ResourcesManager Instance { get; private set; }
-
 
     private void Awake()
     {
@@ -14,7 +18,11 @@ public class ResourcesManager : MonoBehaviour
     }
     #endregion
 
+    [Header("State")]
     [SerializeField] GameState state;
+    /// <summary>
+    /// The actual state of your game
+    /// </summary>
     public GameState State { get => state; }
 
 }
